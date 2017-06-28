ff625d282899d9ae0848999d5c333198469c74f0
chunbaise <chunbaise2016@163.com>
2017-06-28 23:09:24 +0800

    forgot commit PageDownloaderPool interface

--- src/downloader/downloader.go
+++ src/downloader/downloader.go
@@ -12,3 +12,11 @@ type PageDownloader interface {
 type IdGenertor interface {
 	GetUint32() uint32
 }
+
+// 网页下载器池的接口类型
+type PageDownloaderPool interface {
+	Take() (PageDownloader, error)  // 从池中取出一个页面下载器
+	Return(dl PageDownloader) error // 把一个网页下载器归还给池
+	Total() uint32                  // 获得池的总容量
+	Used() uint32                   // 获得正在被使用的下载器数量
+}
